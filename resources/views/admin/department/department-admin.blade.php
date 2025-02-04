e209c0e05126e606ec6c5af731cdbd417b0a2a89
wisnuibroo <wisnuaja0999@gmai.com>
2025-02-04 21:48:19 +0700

    FEAT: [WF] search-department

--- resources/views/admin/department/department-admin.blade.php
+++ resources/views/admin/department/department-admin.blade.php
@@ -12,6 +12,22 @@
                     Add Department
                 </a>
             </div>
+
+            <div class="w-full md:w-1/2">
+                <form class="flex items-center">
+                    <label for="simple-search" class="sr-only">Search</label>
+                    <div class="relative w-full">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"/>
+                            </svg>
+                        </div>
+                        <input type="search" id="default-search"
+                            class="block w-full p-2.5 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Search..." required />
+                    </div>
+                </form>
+            </div>
         </div>
 
             <div class="overflow-hidden">
@@ -98,6 +114,27 @@
 
 
     <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const searchInput = document.getElementById("default-search");
+
+            searchInput.addEventListener("keyup", function() {
+                let filter = searchInput.value.toLowerCase();
+                let rows = document.querySelectorAll("tbody tr");
+
+                rows.forEach(row => {
+                    let name = row.cells[1].textContent.toLowerCase();
+                    let desc = row.cells[2].textContent.toLowerCase();
+
+                    if (name.includes(filter) || desc.includes(filter)) {
+                        row.style.display = "";
+                    } else {
+                        row.style.display = "none";
+                    }
+                });
+            });
+        });
+
+
         //script untuk detail modal
         document.addEventListener("DOMContentLoaded", function(event) {
             // document.getElementById('modalDetail').click();
